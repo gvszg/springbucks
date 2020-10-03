f80e3cf317bad0eca687d4b28fc5904eea5c1b12
jamesyu <zetaentranceserious@gmail.com>
2020-10-03 15:05:16 +0800

    feat: Init Flyway migration

--- src/main/resources/db/migration/V2020.10.03.15.00__create_coffee_table.sql
+++ src/main/resources/db/migration/V2020.10.03.15.00__create_coffee_table.sql
@@ -1,3 +1,14 @@
+drop table if exists t_coffee;
+
+create table if not exists t_coffee (
+    id bigint not null auto_increment,
+    create_time datetime,
+    update_time datetime,
+    name varchar(255),
+    price bigint,
+    primary key (id)
+);
+
 insert into t_coffee (name, price, create_time, update_time) values ('espresso', 2000, now(), now());
 insert into t_coffee (name, price, create_time, update_time) values ('latte', 2500, now(), now());
 insert into t_coffee (name, price, create_time, update_time) values ('capuccino', 2500, now(), now());
